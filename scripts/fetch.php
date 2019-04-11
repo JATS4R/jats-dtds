deb9ee4c790d9aa2ab25a52044519b35e356e2a9
Alf Eaton <eaton.alf@gmail.com>
2019-04-11 08:14:49 +0100

    Initial commit

---
+++ scripts/fetch.php
@@ -0,0 +1,147 @@
+<?php
+
+$curl = curl_init();
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($curl,CURLOPT_CONNECTTIMEOUT, 10);
+curl_setopt($curl, CURLOPT_ENCODING, '');
+
+function build_path($uri) {
+    $parts = parse_url($uri);
+
+//    print_r($parts);
+
+    return "schema/{$parts['host']}{$parts['path']}";
+}
+
+function fetch_url ($curl, $url) {
+    print "$url\n";
+    curl_setopt($curl,CURLOPT_URL, $url);
+    $data = curl_exec($curl);
+
+    if ($errorCode = curl_errno($curl)) {
+        $error = curl_strerror($errorCode);
+        throw new Exception("Error fetching $url: $error");
+    }
+
+    if (!$data) {
+        throw new Exception("No data found at $url");
+    }
+
+    return $data;
+}
+
+$paths = [];
+
+libxml_set_external_entity_loader(function($publicId, $systemId) use ($curl, $catalog, &$paths) {
+    print "$publicId\n";
+
+    if (preg_match('/^schema\/(.+)/', $systemId, $matches)) {
+        $systemId = 'http://' . $matches[1];
+    }
+
+    $path = build_path($systemId);
+
+    if (!file_exists($path)) {
+        $dir = dirname($path);
+
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        $data = fetch_url($curl, $systemId);
+
+        if (!preg_match('/^\s*<!--/', $data)) {
+            throw new Exception("Not a DTD at $systemId");
+        }
+
+//        if (!preg_match('/^<(!--|\?xml|xs:)/', $data)) {
+//            throw new Exception("Not a DTD/XSD at $uri");
+//        }
+
+        file_put_contents($path, $data);
+    }
+
+//    $paths[$publicId] = $path;
+
+    return $path;
+});
+
+$versions = [
+//    '1.0' => '20120330',
+    '1.1d1' => '20130915',
+    '1.1d2' => '20140930',
+    '1.1d3' => '20150301',
+    '1.1' => '20151215',
+    '1.2d1' => '20170631',
+    '1.2d2' => '20180401',
+    '1.2' => '20190208',
+];
+
+$files = [
+    'archiving' => [
+        'archivearticle1' => 'Journal Archiving and Interchange DTD',
+        'archivearticle1-mathml3' => 'Journal Archiving and Interchange DTD with MathML3',
+        'archive-oasis-article1' => 'Journal Archiving and Interchange DTD with OASIS Tables',
+        'archive-oasis-article1-mathml3' => 'Journal Archiving and Interchange DTD with OASIS Tables with MathML3'
+    ],
+    'publishing' => [
+        'journalpublishing1' => 'Journal Publishing DTD',
+        'journalpublishing1-mathml3' => 'Journal Publishing DTD with MathML3',
+        'journalpublishing-oasis-article1' => 'Journal Publishing DTD with OASIS Tables',
+        'journalpublishing-oasis-article1-mathml3' => 'Journal Publishing DTD with OASIS Tables with MathML3'
+    ]
+];
+
+foreach ($files as $colour => $names) {
+    foreach ($versions as $version => $date) {
+        foreach ($names as $name => $title) {
+            $publicId = "-//NLM//DTD JATS (Z39.96) {$title} v{$version} {$date}//EN";
+            $systemId = "http://jats.nlm.nih.gov/{$colour}/{$version}/JATS-{$name}.dtd";
+
+            $xml = <<<XML
+<!DOCTYPE article PUBLIC "$publicId" "$systemId">
+<article/>
+XML;
+            $doc = new DOMDocument;
+            $doc->loadXML($xml, LIBXML_DTDLOAD);
+            $doc->validate();
+
+//            $schema = "https://jats.nlm.nih.gov/{$colour}/{$version}/xsd/JATS-{$name}.xsd";
+//            $doc->schemaValidate($schema);
+
+            $paths[$publicId] = build_path($systemId);
+        }
+
+//        $catalogPath = dirname(build_path($systemID)) . '/catalog.xml';
+//
+//        if (!file_exists($catalogPath)) {
+//            print "$catalogPath\n";
+//            $versionPath = preg_replace('/\./', '-', $version);
+//            $catalogURL = "https://jats.nlm.nih.gov/{$colour}/{$version}/catalog-jats-v{$versionPath}-no-base.xml";
+//            $data = fetch_url($curl, $catalogURL);
+//            file_put_contents($catalogPath, $data);
+//        }
+//
+//        $entry = $catalog->createElement('nextCatalog');
+//        $entry->setAttribute('catalog', $catalogPath);
+//        $catalog->documentElement->appendChild($entry);
+    }
+}
+
+$implementation = new DOMImplementation();
+$dtd = $implementation->createDocumentType('catalog',
+    '-//OASIS//DTD Entity Resolution XML Catalog V1.0//EN',
+    'http://www.oasis-open.org/committees/entity/release/1.0/catalog.dtd');
+$catalog = $implementation->createDocument('urn:oasis:names:tc:entity:xmlns:xml:catalog', 'catalog', $dtd);
+$catalog->documentElement->setAttribute('prefer', 'public');
+
+foreach ($paths as $publicId => $path) {
+    $entry = $catalog->createElement('public');
+    $entry->setAttribute('publicId', $publicId);
+    $entry->setAttribute('uri', $path);
+    $catalog->documentElement->appendChild($entry);
+}
+
+$catalog->encoding = 'utf-8';
+$catalog->formatOutput = true;
+$catalog->save('catalog.xml');
